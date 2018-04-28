efa39460aae4cd0716cd695630ff03b92633266a
Piotr Jaromin <piotr.jaromin@performgroup.com>
2018-04-28 23:33:26 +0200

    working menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('./../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('./../node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./styles/main.scss');
 
 const React = window.React = require('react'),
